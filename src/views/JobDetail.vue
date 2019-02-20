fee8550ec26e7d5685004cc594de31fdd38943d1
ityoung <ityoung@foxmail.com>
2019-02-20 15:24:42 +0800

    feat: get pipeline

--- src/views/JobDetail.vue
+++ src/views/JobDetail.vue
@@ -13,10 +13,10 @@
             <Col span="8">
                 <h2>Version xxx</h2>
                 <Divider/>
-                bbb
+                步骤选择:
                 <br/>
-                <Select v-model="model1" style="width:200px">
-                    <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+                <Select v-model="defautOption" style="width:200px" @on-change="getJobListOfStage">
+                    <Option v-for="item in pipeLineList" :value="item" :key="item">{{ item }}</Option>
                 </Select>
                 <Divider/>
                 <ul style="list-style-type: none">
@@ -45,21 +45,35 @@
         },
         data() {
             return {
-                cityList: [
-                    {
-                        value: 'New York',
-                        label: 'New York'
-                    },
-                    {
-                        value: 'London',
-                        label: 'London'
-                    },
-                    {
-                        value: 'Sydney',
-                        label: 'Sydney'
-                    },
-                ],
-                model1: ''
+                project_id: this.$route.params.project_id,
+                version: this.$route.params.version,
+                pipeLineList: [],
+                // 下拉框默认选项
+                defautOption: ''
+            }
+        },
+        created() {
+            this.getPipeline();
+        },
+        methods: {
+            getPipeline() {
+                // 获取该版本的Pipeline列表
+                var vm = this;
+                vm.$http
+                    .get(`http://0.0.0.0:5000/api/version/get-pipeline?project=${vm.project_id}&version=${vm.version}`)
+                    .then(res => {
+                        vm.pipeLineList = res.data.data.pipeline;
+                        vm.defautOption = vm.pipeLineList[0]
+                    })
+            },
+            getJobListOfStage(option) {
+                // 获取某一步骤的所有Job
+                debugger;
+                console.log(option)
+            },
+            getJobDetail() {
+                // 获取某一Job的具体信息
+
             }
         }
     }
