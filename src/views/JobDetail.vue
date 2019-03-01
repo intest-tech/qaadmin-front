863680f0afa52f2cd6a30f8bcd0fbd8d91cf47c2
ityoung <ityoung@foxmail.com>
2019-03-01 11:39:04 +0800

    feat: styling jobDetail card

--- src/views/JobDetail.vue
+++ src/views/JobDetail.vue
@@ -1,7 +1,7 @@
 <template>
     <!--<VersionSteps></VersionSteps>-->
     <Card style="height: 100%">
-        <Row :gutter="16" style="height: 100%" >
+        <Row :gutter="16" style="height: 100%">
             <Col span="16" style="height: 100%">
                 <Card style="height: 100%">
                     <Tabs>
@@ -15,18 +15,21 @@
             </Col>
             <Col span="8" style="height: 100%">
                 <Card style="height: 100%">
-                    <h2>Version {{ version }}</h2>
+                    <h2>{{ jobDetail.stage }}</h2>
+                    <Divider/>
+                    <p>Project: {{ project_id }}</p>
+                    <p>Version: {{ version }}</p>
+                    <p>Duration: {{ parseFloat(jobDetail.duration).toFixed(3) }} s</p>
+                    <p>Tag: {{ jobDetail.tag }}</p>
                     <Divider/>
                     步骤选择:
-                    <br/>
                     <Select v-model="defautOption" style="width:200px" @on-change="getJobListOfStage">
                         <Option v-for="item in pipeLineList" :value="item" :key="item">{{ item }}</Option>
                     </Select>
                     <Divider/>
-                    <Timeline>
-                        <TimelineItem v-for="item in jobList" :key="item._id.$oid" color="green"><a
-                                @click="getJobDetails(item._id.$oid)">{{ item.stage }}</a></TimelineItem>
-                    </Timeline>
+                    <!--TODO: 过长会超出card高度-->
+                    <Table highlight-row :columns="tablecolumns" :data="jobList" :show-header="false"
+                           @on-row-click="getJobDetails"></Table>
                 </Card>
             </Col>
         </Row>
@@ -34,7 +37,7 @@
 </template>
 
 <script>
-    import Traceback from '@/components/job/Traceback.vue'
+    import Traceback from '@/components/job/Traceback.vue';
     import {getVersionDetails} from "../apis/version";
     import {getJobList, getJobDetails} from "../apis/job"
 
@@ -54,7 +57,33 @@
                 jobList: [],
                 defautJob: '',
                 // 任务详情
-                jobDetail: {}
+                jobDetail: {},
+                //任务列表表格配置
+                tablecolumns: [
+                    {
+                        title: 'Status',
+                        key: 'was_successful',
+                        maxWidth: 120,
+                        render: (h, params) => {
+                            const row = params.row;
+                            // const color = row.status === 1 ? 'primary' : row.status === 2 ? 'success' : 'error';
+                            // const text = row.status === 1 ? 'Working' : row.status === 2 ? 'Success' : 'Fail';
+                            const color = row.status === true ? 'success' : 'error';
+                            const text = row.status === true ? 'Success' : '\b\b\bError\b\b\b';
+
+                            return h('Tag', {
+                                props: {
+                                    color: color,
+                                }
+                            }, text);
+                        }
+                    },
+                    {
+                        title: 'Stage',
+                        key: 'stage',
+                        tooltip: 'true'
+                    }
+                ]
             }
         },
         created() {
@@ -76,14 +105,15 @@
                 getJobList(vm.project_id, vm.version, option).then(([err, data, res]) => {
                     vm.jobList = data;
                     if (vm.jobList.length !== 0) {
-                        vm.defautJob = vm.jobList[0]._id.$oid;
+                        vm.defautJob = vm.jobList[0];
                         vm.getJobDetails(vm.defautJob);
                     }
                 })
             },
-            getJobDetails(id) {
+            getJobDetails(data) {
                 // 获取某一Job的具体信息
                 var vm = this;
+                var id = data._id.$oid;
                 getJobDetails(id).then(([err, data, res]) => {
                     vm.jobDetail = data.data;
                 });
@@ -93,8 +123,8 @@
 </script>
 
 <style>
-.ivu-card-body{
-    padding: 16px;
-    height: 100%;
-}
+    .ivu-card-body {
+        padding: 16px;
+        height: 100%;
+    }
 </style>
