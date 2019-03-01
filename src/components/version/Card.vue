44e5888cab3bee53d123ed57a7e110d75efea68d
ityoung <ityoung@foxmail.com>
2019-03-01 09:20:37 +0800

    feat: styling version card

--- src/components/version/Card.vue
+++ src/components/version/Card.vue
@@ -1,9 +1,20 @@
 <template>
     <div style="background:#eee;padding: 1px">
         <Card :bordered="true">
-            <router-link :to="{ path: `/project/${project_id}/v/${version}` }">{{ version }}</router-link>
-            <p>Content of no border type. Content of no border type. Content of no border type. Content of no border
-                type. </p>
+            <Row>
+                <Col span="6">
+                    TODO: status
+                </Col>
+                <Col span="6">
+                    <router-link :to="{ path: `/project/${project_id}/v/${version}` }">{{ version }}</router-link>
+                </Col>
+                <Col span="6">
+                    TODO: Pipeline
+                </Col>
+                <Col span="6">
+                    TODO: datetime
+                </Col>
+            </Row>
         </Card>
     </div>
 </template>
@@ -13,7 +24,7 @@
         name: "VersionCard",
         props: ['version'],
         data() {
-            return{
+            return {
                 project_id: this.$route.params.project_id
             }
         }
